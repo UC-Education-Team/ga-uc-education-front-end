59bd745aec5c1778a87c47a5fee7550b8584a9fd
zendrias <zenas@live.com>
2022-05-04 14:52:52 -0400

    continue quiz form

--- src/pages/CreateQuiz/CreateQuiz.jsx
+++ src/pages/CreateQuiz/CreateQuiz.jsx
@@ -6,15 +6,19 @@ function CreateQuiz(props) {
   const [validForm, setValidForm] = useState(false)
   const [correctAnswers, setCorrectAnswers] = useState(false)
   const formElement = useRef()
+  const question1Options = useRef()
   function handleChange(e) {
     setFormData({ ...formData, [e.target.name]: e.target.value })
     console.log(formData)
   }
 
   function correctAnswer(e) {
-    setCorrectAnswers({ ...correctAnswers, [e.target.id]: e.target.className })
-    e.target.className === 'true' ? e.target.className = 'false' : e.target.className = 'true'
-    console.log(correctAnswers)
+    setCorrectAnswers({ ...correctAnswers, [e.target.id]: e.target.name })
+    console.log(question1Options)
+    question1Options.current.childNodes.forEach(node => (
+      node.localName === 'input' ? node.style.border = '1px solid black' : console.log(node.localName)
+    ))
+    e.target.style.border = '4px solid green'
   }
 
   useEffect(() => {
@@ -25,23 +29,31 @@ function CreateQuiz(props) {
     <div>
       <h1>This is a quiz</h1>
       <form ref={formElement} onSubmit={(e) => e.preventDefault()}>
-        {/* Lesson Name*/}
+        {/* Question 1 */}
         <p>Question 1</p>
         <textarea
           required
-          name="a"
+          name="question1"
           id="question1"
           cols="40"
           rows="3"
           onChange={handleChange}
         >{formData.question1}</textarea>
-        <input
-          onChange={handleChange}
-          onDoubleClick={correctAnswer}
-          className='0'
-          id="question1"
-          type="text" />
+        <div ref={question1Options}>
+          {['a', 'b', 'c', 'd'].map(ltr => (
+            <>
+              <p>Answer Choice {ltr.toUpperCase()}</p>
+              <input
+                onChange={handleChange}
+                onDoubleClick={correctAnswer}
+                name={`${ltr}`}
+                id="question1"
+                type="text" />
+            </>
+          ))}
+        </div>
 
+        {/* Question 2 */}
         <p>Question 2</p>
         <textarea
           required
@@ -51,6 +63,22 @@ function CreateQuiz(props) {
           rows="3"
           onChange={handleChange}
         >{formData.question2}</textarea>
+
+        <div ref={question1Options}>
+          {['a', 'b', 'c', 'd'].map(ltr => (
+            <>
+              <p>Answer Choice {ltr.toUpperCase()}</p>
+              <input
+                onChange={handleChange}
+                onDoubleClick={correctAnswer}
+                name={`${ltr}`}
+                id="question1"
+                type="text" />
+            </>
+          ))}
+        </div>
+
+        {/* Question 3 */}
         <p>Question 3</p>
         <textarea
           required
@@ -60,6 +88,22 @@ function CreateQuiz(props) {
           rows="3"
           onChange={handleChange}
         >{formData.question3}</textarea>
+
+        <div ref={question1Options}>
+          {['a', 'b', 'c', 'd'].map(ltr => (
+            <>
+              <p>Answer Choice {ltr.toUpperCase()}</p>
+              <input
+                onChange={handleChange}
+                onDoubleClick={correctAnswer}
+                name={`${ltr}`}
+                id="question1"
+                type="text" />
+            </>
+          ))}
+        </div>
+
+        {/* Question 4 */}
         <p>Question 4</p>
         <textarea
           required
@@ -69,6 +113,22 @@ function CreateQuiz(props) {
           rows="3"
           onChange={handleChange}
         >{formData.question4}</textarea>
+
+        <div ref={question1Options}>
+          {['a', 'b', 'c', 'd'].map(ltr => (
+            <>
+              <p>Answer Choice {ltr.toUpperCase()}</p>
+              <input
+                onChange={handleChange}
+                onDoubleClick={correctAnswer}
+                name={`${ltr}`}
+                id="question1"
+                type="text" />
+            </>
+          ))}
+        </div>
+
+        {/* Question 5 */}
         <p>Question 5</p>
         <textarea
           required
@@ -78,6 +138,21 @@ function CreateQuiz(props) {
           rows="3"
           onChange={handleChange}
         >{formData.question5}</textarea>
+
+        <div ref={question1Options}>
+          {['a', 'b', 'c', 'd'].map(ltr => (
+            <>
+              <p>Answer Choice {ltr.toUpperCase()}</p>
+              <input
+                onChange={handleChange}
+                onDoubleClick={correctAnswer}
+                name={`${ltr}`}
+                id="question1"
+                type="text" />
+            </>
+          ))}
+        </div>
+
         <button disabled={!validForm} type="submit">Create Lesson</button>
       </form>
     </div>
