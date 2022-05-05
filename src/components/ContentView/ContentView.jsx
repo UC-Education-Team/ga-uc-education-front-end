5b52960c0adf68c63d39bb7bf4b02004b7f1e9ed
tristonpalacios <triston.j.palacios@gmail.com>
2022-05-05 14:28:41 -0700

    trivia fact in view

--- src/components/ContentView/ContentView.jsx
+++ src/components/ContentView/ContentView.jsx
@@ -1,6 +1,7 @@
 import Question from '../Modules/Vector.png'
 import ModuleCard from "../ModuleCard/ModuleCard";
 import { Card, ProgressBar, Carousel } from "react-bootstrap";
+import art from './image 3.png' 
 
 const ContentView = ({modules, progressBarFun, ControlledCarousel, lessonSelect, lessonSelection, lessons, moduleSelect}) => {
 
@@ -31,20 +32,11 @@ const ContentView = ({modules, progressBarFun, ControlledCarousel, lessonSelect,
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor amet suspendisse nisi vulputate mauris ut.</p>
 
                 </div>
-                <div>
-                <Card className="ProgressCard">
-                    <h1 style={{ fontSize: '1.3rem', textAlign: 'center', marginBottom: '4vh', marginTop: '15px' }}>Learning Progress</h1>
-                    {modules.map((module) => {
-                    const now = Math.floor(Math.random() * 100)
-                    return (
-                        <div className="ProgressItem">
-                        <h1 className="progressname">{module.name}</h1>
-                        <div className="ProgressAndLabel">{progressBarFun(module.number)}  <p className="ProgressLabel"> {module.number}%</p></div>
-
-                        <hr />
-                        </div>
-                    );
-                    })}
+                <div style={{width:'20vw', marginRight:'none'}}>
+                <Card className="ProgressCard" style={{width:'30vw', marginRight:'none'}}>
+                    <h1 style={{ fontSize: '1.3rem', textAlign: 'center', marginTop: '15px' }}>Unlock Communities</h1>
+                    <h2 style={{ fontSize: '1.3rem', textAlign: 'center', marginBottom: '4vh', marginTop: '15px' }}>Trivia Facts!</h2>
+                    <p style={{display:'flex', fontSize:'20px',}}> Do you know, Unlocking Communities has made a positive impact in over 130,000 peoples lives since 2018? <img src={art} alt="" /> </p>
                 </Card>
                 </div>
             </div>
